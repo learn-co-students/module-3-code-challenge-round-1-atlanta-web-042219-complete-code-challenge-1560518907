bf277a4d3745e638f5eb91fc53b5ba1a7028a6ee
Paula Tulis <tulis.paula@gmail.com>
2019-06-14 11:00:12 -0400

    not done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,88 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+let imageId = 2842
+const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+const likeURL = `https://randopic.herokuapp.com/likes/`
+const commentsURL = `https://randopic.herokuapp.com/comments/`
+const imageCard = document.querySelector('#image_card')
+let likeArea = document.querySelector('#likes')
+let commentBar = document.querySelector('#comments')
+let commentForm = document.querySelector('#comment_form')
+showComments()
 
-  let imageId = 1 //Enter the id from the fetched image here
+document.addEventListener('click', handleEvent)
+imageCard.addEventListener('submit', handleSubmit)
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+fetch(imageURL)
+.then(res =>res.json())
+.then(showImage)
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+function showImage(image){
+  imageCard.innerHTML =
+  `<img src="${image.url}" id="image" data-id="${image.id}"/>
+  <h4 id="name">${image.name}</h4>
+  <span>Likes:
+    <span id="likes">${image.like_count}</span>
+  </span>
+  <button id="like_button">Like</button>
+  <form id="comment_form">
+    <input id="comment_input" type="text" name="comment" value=" " placeholder="Add Comment"/>
+    <input type="submit" value="Submit"/>
+  </form>
+  <ul id="comments">${commentBar.innerHTML}</ul>
+  `
+}
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+function showComments(comment){
+  commentBar.innerHTML = `
+       <li>${image.comments? image.comments : 'No Comments Yet'}</li>`
+}
+function handleEvent(e){
+  if (e.target.innerHTML === 'Like'){
+    increaseLikeCount(e)
+  }
+}
+
+function increaseLikeCount(e){
+
+  let imageId = e.target.offsetParent.childNodes[0].dataset.id
+  let updatedLikes = ++ e.target.offsetParent.childNodes[4].childNodes[1].innerText
+
+  likeArea.innerHTML = `${updatedLikes}`
+
+  fetch(likeURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accepts: 'application/json'
+    },
+    body: JSON.stringify({
+       image_id: imageId
+    })
+  })
+  .then(res => res.json())
+}
+
+function handleSubmit(e){
+  e.preventDefault()
+  // console.log(e)
+  let imageID = e.target.parentElement.children[0].attributes[2].nodeValue
+  // console.log(commentForm.parentElement)
+  // let content = something I cannot figure out atm, unfortunately should be innerTEXT of comment-input
+
+  fetch(commentsURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accepts: 'application/json'
+    },
+    body: JSON.stringify({
+      image_id: imageId,
+      content: content
+    })
+  }).then(res => res.json())
+
+
+
+}
 
 })
